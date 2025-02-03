2ab95092c81408d0d3757d6e1ab75f5dd176e448
Lawrence_3067 <chaulawrencejh@gmail.com>
2025-02-03 22:38:47 +0000

    updated student_gtests

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -24,37 +24,99 @@ TEST(StringFunction, strcpy) {
 }
 
 TEST(StringFunction, strdup) {
-    EXPECT_TRUE(false);
+    EXPECT_EQ(String::strdup("jello"), "jello");
+    EXPECT_EQ(String::strdup(), "");
 }
 
 TEST(StringFunction, strncpy) {
-    EXPECT_TRUE(false);
+    char result[10];
+
+    EXPECT_EQ(String::strncpy(result, "bruh", 3), result);
+    EXPECT_STREQ(result, "bru");
+
+    EXPECT_EQ(String::strcpy(result, ""), result);
+    EXPECT_STREQ(result, "");
 }
 
 TEST(StringFunction, strcat) {
-    EXPECT_TRUE(false);
+    char result[30]{"yes"};
+
+    char first[10]{"Hello"};
+    char second[10]{" World!"};
+    String s{"Hello"};
+
+    EXPECT_STREQ(String::strcat(result, first), "yesHello");
+    EXPECT_STREQ(String::strcat(result, second), "yesHello World!");
+    EXPECT_STREQ(first, "Hello");
+    EXPECT_STREQ(second, " World!");
 }
 
 TEST(StringFunction, strncat) {
-    EXPECT_TRUE(false);
+    char result[30]{"yes"};
+    char first[10]{"Hello"};
+    char second[10]{" World!"};
+
+    EXPECT_STREQ(String::strncat(result, first, 3), "yesHel");
+    EXPECT_STREQ(String::strncat(result, second, 4), "yesHel Wor");
+    EXPECT_STREQ(String::strncat(result, second, 0), "yesHel Wor");
+    EXPECT_STREQ(first, "Hello");
+    EXPECT_STREQ(second, " World!");
 }
 
 TEST(StringFunction, strcmp) {
-    EXPECT_TRUE(false);
+    char first[10]{"Hello"};
+    char second[10]{"Hello"};
+    char third[10]{"hello"};
+
+    EXPECT_EQ(String::strcmp(first, second), 0);
+    EXPECT_EQ(String::strcmp(first, third), -32);
+    EXPECT_EQ(String::strcmp(third, first), 32);
 }
 
 TEST(StringFunction, strncmp) {
-    EXPECT_TRUE(false);
+    char first[10]{"foo"};
+    char second[10]{"foobar"};
+    char third[10]{""};
+    char fourth[10]{""};
+
+    EXPECT_EQ(String::strncmp(first, second, 3), 0);
+    EXPECT_TRUE(String::strncmp(first, second, 4) < 0);
+    EXPECT_TRUE(String::strncmp(second, first, 4) > 0);
+    EXPECT_EQ(String::strncmp(third, fourth, 5), 0);
+    EXPECT_TRUE(String::strncmp(third, first, 2) < 0);
+    EXPECT_TRUE(String::strncmp(first, third, 2) > 0);
+    EXPECT_TRUE(String::strncmp(first, second, 10) < 0);
+    EXPECT_TRUE(String::strncmp(second, first, 10) > 0);
+    EXPECT_EQ(String::strncmp(first, first, 3), 0);
 }
 
 TEST(StringFunction, reverse_cpy) {
-    EXPECT_TRUE(false);
+    char first[10]{"Hello"};
+    char result[10];
+
+    String::reverse_cpy(result, first);
+    EXPECT_STREQ(result, "olleH");
 }
 
 TEST(StringFunction, strchr) {
-    EXPECT_TRUE(false);
+    char first[10]{"Hello"};
+
+    EXPECT_STREQ(String::strchr(first, 'e'), "ello");
+    EXPECT_STREQ(String::strchr(first, 'l'), "llo");
 }
 
 TEST(StringFunction, strstr) {
-    EXPECT_TRUE(false);
+    char first[30]{"HelHellHellolo"};
+    char second[30]{"ababababab"};
+    char third[10]{""};
+    char tester[10]{"c"};
+    
+    EXPECT_STREQ(String::strstr(first, "Hello"), "Hellolo");
+    EXPECT_STREQ(String::strstr(first, "lH"), "lHellHellolo");
+    EXPECT_STREQ(String::strstr(second, "abababab"), "ababababab");
+    EXPECT_FALSE(String::strstr(first, "lH") == "HelHellHello");
+    EXPECT_EQ(String::strstr(first, tester), nullptr);
+    EXPECT_EQ(String::strstr(first, ""), first);
+    EXPECT_EQ(String::strstr(third, tester), nullptr);
+    EXPECT_EQ(String::strstr(third, ""), third);
 }
